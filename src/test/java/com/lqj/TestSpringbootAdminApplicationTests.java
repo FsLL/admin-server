fd53fb228bb4909e5e62b73e4509f12893caf8bd
FsLL <412470220@qq.com>
2023-01-09 16:13:07 +0800

    1.Springboot Admin 服务端首次提交

---
+++ src/test/java/com/lqj/TestSpringbootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lqj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringbootAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
